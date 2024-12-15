5d717cd7ef6f634c46215e7c048f56c2ebeb4954
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2024-12-15 07:35:10 -0800

    Delete src/app directory

--- src/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="bg-black  py-4 text-center border-t border-[#C1BAA1]">
-            <p className="text-white font-semibold ${montserrat.className} mt-2"> Urooj Saeed &copy; 2025. All rights reserved.</p>
-        </footer>
-    );
-};
-
-export default Footer;
