a8ea57181a9d26820533c4cacc294b01d2b985c9
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2024-12-15 07:32:08 -0800

    Add files via upload

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-black  py-4 text-center border-t border-[#C1BAA1]">
+            <p className="text-white font-semibold ${montserrat.className} mt-2"> Urooj Saeed &copy; 2025. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
